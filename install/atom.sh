5049679299be598d5d17a14dc6ce6ce3732b53b2
cfalcondair <cfalcondair@gmail.com>
2019-04-17 17:13:43 +0100

    Clean out the atom packages that we don't need

--- install/atom.sh
+++ install/atom.sh
@@ -5,24 +5,10 @@ set -euo pipefail
 echo "Installing atom packages"
 
 packages=(
-  Hydrogen
-  autocomplete-json
-  autocomplete-modules
-  autocomplete-python
-  busy-signal
-  docblock-python
-  go-debug
-  go-plus
-  intentions
+  language-markdown
   language-scala
   language-terraform
-  linter
-  linter-scalac
-  linter-terraform-syntax
-  linter-ui-default
   package-settings
-  scala-format
-  teletype
 )
 
 
